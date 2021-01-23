6b6afb1757a5ac12ce7574484326773b2f672466
Jan van Oorschot <janvanoorschot@gmail.com>
2021-01-23 15:05:42 +0100

    Small thingie.

--- client/__init__.py
+++ client/__init__.py
@@ -1,5 +1,4 @@
 from .rest_client import RestClient
-from .umpire import CLIUmpire
 from .player import CLIPlayer
 from .local_player import CLILocalPlayer
 from .engine_handler import LocalEngineHandler, RemoteEngineHandler
