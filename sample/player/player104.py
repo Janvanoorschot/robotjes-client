5bab8ba9da86b1bd3aba6e19a54d1c6a95799695
Jan van Oorschot <janvanoorschot@gmail.com>
2021-02-05 13:12:38 +0100

    Multi-forward example

---
+++ sample/player/player104.py
@@ -0,0 +1,23 @@
+
+
+def execute(robo):
+    while True:
+        robo.forward(2)
+        robo.noop()
+        robo.right()
+        robo.forward()
+        robo.left()
+        robo.forward(2)
+        robo.noop()
+        robo.left(2)
+        robo.noop()
+        robo.forward(2)
+        robo.noop()
+        robo.right()
+        robo.forward()
+        robo.left()
+        robo.forward(2)
+        robo.noop()
+        robo.left(2)
+        robo.noop()
+
