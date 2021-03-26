f024b6368a04e2503016120d1a5e057204aa4c6a
Jan van Oorschot <janvanoorschot@gmail.com>
2021-03-26 20:35:04 +0100

    Starting on field/robo_game. These will connect to engine.

---
+++ robotjes/server/field.py
@@ -0,0 +1,108 @@
+from robotjessrv.server.bubblehub.model import GameSpec
+from . import GameStatus, RoboGame, Player
+
+
+class Field:
+    """
+        A Field where players/robots can check-in/check-out
+        at will
+    """
+
+    def __init__(self, owner, spec: GameSpec):
+        self.owner = owner
+        self.game = self.create_game(spec)
+        self.game_name = spec.game_name
+        self.isStarted = False
+        self.isStopped = False
+        self.isSuccess = False
+        self.tick = 0
+        self.game_tick = 0
+        self.max_player_count = 10
+        self.max_start_tick = 15
+        self.max_tick = 30
+        self.players = {}
+        self.resolution = 5
+
+    def create_game(self, spec: GameSpec):
+        # for the time being only create RoboGame's
+        mapstr = self.owner.mazes.get_map(spec.maze_id)
+        return RoboGame(mapstr)
+
+    def created(self):
+        # send a status change to the games exchange
+        self.owner.publish(GameStatus.CREATED, {
+            "maze_map": self.game.maze_map()
+        })
+        # we start the game immediatly after it is created
+        self.owner.start_game()
+
+    def started(self):
+        self.game_tick = 0
+        self.isStarted = True
+        self.isStopped = False
+        self.isSuccess = True
+        self.owner.publish(GameStatus.STARTED, {})
+
+    def stopped(self):
+        self.owner.publish(GameStatus.STOPPED, {})
+
+    def registered(self, player_id, player_name):
+        player = Player(player_id, player_name)
+        robo_id = self.game.create_robo(player.player_id)
+        if robo_id:
+            player.robos.append(robo_id)
+            self.players[player_id] = player
+            return True
+        else:
+            return False
+
+    def deregistered(self, player_id):
+        if player_id in self.players:
+            player = self.players[player_id]
+            for robo_id in player.robos:
+                self.game.destroy_robo(robo_id)
+            del self.players[player_id]
+
+    def is_stopped(self):
+        return self.isStopped
+
+    def result(self):
+        return self.isSuccess
+
+    def player_count(self):
+        return self.max_player_count
+
+    def get_game_status(self):
+        return {
+            "game_tick": self.game_tick,
+            "isStarted": self.isStarted,
+            "isStopped": self.isStopped,
+            "isSuccess": self.isSuccess
+        }
+
+    def get_player_status(self, player_id):
+        if player_id in self.players:
+            player = self.players[player_id]
+            return player.get_status(self.game)
+        else:
+            return {}
+
+    def get_map_status(self):
+        return self.game.get_map_status()
+
+    def timer(self, tick):
+        self.tick = tick
+
+    def game_timer(self, tick, moves):
+        self.game_tick = self.game_tick + 1
+        self.game.start_moves(self.game_tick)
+        for player_id, move in moves.items():
+            line_no = move[0]
+            robo_id = move[1]
+            self.game.execute(self.game_tick, robo_id, move)
+        self.game.end_moves(self.game_tick)
+        # publish info: GAMETICK AND/OR DELTAREC
+        self.owner.publish(GameStatus.GAMETICK, {})
+        if self.game_tick % self.resolution == 0:
+            drec = self.game.recording_delta()
+            self.owner.publish(GameStatus.DELTAREC, drec)
---
+++ robotjes/server/robo_game.py
@@ -0,0 +1,65 @@
+from robotjes.sim import Engine, Map
+
+class RoboGame:
+    """ Robotjes specific game behaviour. """
+    def __init__(self, mapstr):
+        self.robos = {}
+        self.map = Map.fromstring(mapstr)
+        self.engine = Engine(self.map)
+        self.game_tick = 0
+        self.last_recording_delta = 0
+
+    def create_robo(self, player_id):
+        robo_id = self.engine.create_robo()
+        if robo_id:
+            self.robos[robo_id] = {
+                'player': player_id
+            }
+            return robo_id
+        else:
+            return None
+
+    def destroy_robo(self, robo_id):
+        self.engine.destroy_robo(robo_id)
+        del self.robos[robo_id]
+
+    def start_moves(self, game_tick):
+        self.game_tick = game_tick
+        self.engine.game_timer(game_tick)
+
+    def execute(self, game_tick, robo_id, move):
+        # execute the move for the given robo
+        self.engine.execute(game_tick, robo_id, move)
+
+    def end_moves(self, game_tick):
+        pass
+
+    def get_status(self, robo_id):
+        return self.engine.get_status(robo_id)
+
+    def recording_delta(self):
+        # get the frames since the last time we asked, plus our current map_status
+        frames = self.engine.get_recording().toMapFrom(self.last_recording_delta)
+        self.last_recording_delta = self.last_recording_delta + len(frames)
+        map_status = self.get_map_status()
+        # combine the frame of one timeslot together
+        combined_frames = []
+        ix = 0
+        while ix < len(frames):
+            frame = []
+            cur_tick = frames[ix]['tick']
+            while ix < len(frames) and frames[ix]['tick'] == cur_tick:
+                frame.append(frames[ix])
+                ix = ix + 1
+            combined_frames.append(frame)
+        return {
+            "game_tick": self.game_tick,
+            "frames": combined_frames,
+            "map_status": map_status
+        }
+
+    def maze_map(self):
+        return self.map.toMazeMap()
+
+    def get_map_status(self):
+        return self.engine.get_map_status()
