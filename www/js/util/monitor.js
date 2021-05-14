4020122768dd1e2bf84a870b227f58e6ca68c2a0
Jan van Oorschot <janvanoorschot@gmail.com>
2021-05-14 16:08:54 +0200

    ongoing

--- www/js/util/monitor.js
+++ www/js/util/monitor.js
@@ -1,68 +1,72 @@
 'use strict';
-(function($) {
+const MONTYPE_GAMETICK = 1;
 
-    /** initialise namespace */
+const MONMSG_SERVER = 2;
 
-    $.fn.startmon = function() {
-        if($.fn.monitor.singleton === undefined) {
+/** initialise namespace */
+
+(function ($) {
+
+    $.fn.startmon = function () {
+        if ($.fn.monitor.singleton === undefined) {
             $.fn.monitor.singleton = $.fn.monitor.handler();
             $.fn.monitor.singleton.start();
         }
 
     };
 
-    $.fn.stopmon  = function() {
-        if($.fn.monitor.singleton !== undefined) {
+    $.fn.stopmon = function () {
+        if ($.fn.monitor.singleton !== undefined) {
             $.fn.monitor.singleton.stop();
             $.fn.monitor.singleton = undefined;
         }
 
     };
 
-    $.fn.registermon  = function(listener) {
-        if($.fn.monitor.singleton !== undefined) {
+    $.fn.registermon = function (listener) {
+        if ($.fn.monitor.singleton !== undefined) {
             $.fn.monitor.singleton.addListener(listener);
         }
     };
 
-    $.fn.deregistermon  = function(listener) {
-        if($.fn.monitor.singleton !== undefined) {
+    $.fn.deregistermon = function (listener) {
+        if ($.fn.monitor.singleton !== undefined) {
             $.fn.monitor.singleton.removeListener(listener);
         }
     };
 
-    $.fn.genmon = function(type, msg) {
-        if($.fn.monitor.singleton !== undefined) {
+    $.fn.genmon = function (type, msg) {
+        if ($.fn.monitor.singleton !== undefined) {
             $.fn.monitor.singleton.genmon(type, msg, arguments);
         }
     };
 
-    $.fn.monitor.handler = function() {
+    $.fn.monitor.handler = function () {
         let that = {};
 
         that.listeners = [];
 
-        that.start = function() {
+        that.start = function () {
         }
 
-        that.stop = function() {
+        that.stop = function () {
         }
 
-        that.addListener = function(listener) {
+        that.addListener = function (listener) {
             that.listeners.push(listener);
         }
 
-        that.removeListener = function(listener) {
+        that.removeListener = function (listener) {
             const index = that.listeners.indexOf(listener);
             if (index > -1) {
                 that.listeners.splice(index, 1);
             }
         }
 
-        that.genmon = function(type, msg, args) {
+        that.genmon = function (type, msg, args) {
             that.listeners.forEach(listener => {
-                if(listener.hasOwnProperty('genmon')) {
-                    listener.genmon(type, msg, args);
+                if (typeof listener === 'function') {
+                    listener(type, msg, args);
                 }
             });
         }
